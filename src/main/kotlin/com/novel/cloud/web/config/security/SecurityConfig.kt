e7bbad27db61b1a8ebca585174c8eca21d43f064
youngmin <min050410@gmail.com>
2023-03-14 14:17:21 +0900

    refac: securityConfig

--- src/main/kotlin/com/novel/cloud/web/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/novel/cloud/web/config/security/SecurityConfig.kt
@@ -4,20 +4,18 @@ import com.novel.cloud.web.config.security.jwt.JwtOncePerRequestFilter
 import com.novel.cloud.web.path.ApiPath
 import com.novel.cloud.web.security.CustomOauth2SuccessHandler
 import com.novel.cloud.web.security.CustomOauth2UserService
-import lombok.RequiredArgsConstructor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.    config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 class SecurityConfig(
     private val jwtOncePerRequestFilter: JwtOncePerRequestFilter,
     private val customAuthenticationEntryPoint: CustomAuthenticationEntryPoint,
@@ -27,32 +25,34 @@ class SecurityConfig(
 ) {
 
     @Bean
-    @Throws(Exception::class)
-    fun apiFilterChain(http: HttpSecurity): SecurityFilterChain? {
-        http.httpBasic().disable()
-        http.formLogin().disable()
-        http.csrf().disable()
-        http.cors()
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        http.authorizeRequests()
-            .antMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll()
-            .antMatchers("/resources/**", "/").permitAll() // 에러 핸들러
-            .antMatchers(ApiPath.ERROR_AUTH).permitAll() // 인증
-            .antMatchers(ApiPath.LOGIN_OAUTH2, ApiPath.REFRESH_TOKEN).permitAll()
-            .anyRequest().authenticated()
-        http.addFilterBefore(jwtOncePerRequestFilter, UsernamePasswordAuthenticationFilter::class.java)
+    fun apiFilterChain(http: HttpSecurity): SecurityFilterChain = with(http) {
+
+        httpBasic().disable()
+        formLogin().disable()
+        csrf().disable()
+        cors()
+        sessionManagement {
+            it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        }
+        authorizeRequests {
+            it.antMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll()
+            it.antMatchers("/resources/**", "/").permitAll() // 에러 핸들러
+            it.antMatchers(ApiPath.ERROR_AUTH).permitAll() // 인증
+            it.antMatchers(ApiPath.LOGIN_OAUTH2, ApiPath.REFRESH_TOKEN).permitAll()
+            it.anyRequest().authenticated()
+        }
+        addFilterBefore(jwtOncePerRequestFilter, UsernamePasswordAuthenticationFilter::class.java)
             .exceptionHandling()
             .authenticationEntryPoint(customAuthenticationEntryPoint)
             .accessDeniedHandler(customAccessDeniedHandler)
-        http.oauth2Login()
-            .successHandler(customOauth2SuccessHandler)
-            .userInfoEndpoint()
-            .userService(customOauth2UserService)
-        return http.build()
+        oauth2Login {
+            it.successHandler(customOauth2SuccessHandler)
+            it.userInfoEndpoint().userService(customOauth2UserService)
+        }
+        return build()
     }
 
     @Bean
-    @Throws(Exception::class)
     fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager? {
         return authenticationConfiguration.authenticationManager
     }
--- src/main/kotlin/com/novel/cloud/web/config/web/WebConfig.kt
+++ src/main/kotlin/com/novel/cloud/web/config/web/WebConfig.kt
@@ -2,7 +2,6 @@ package com.novel.cloud.web.config.web
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
