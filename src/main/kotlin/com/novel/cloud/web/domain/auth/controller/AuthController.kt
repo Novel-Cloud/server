c9f2647413f5b76ad9cf78b3ec6d958d7127930a
youngmin <min050410@gmail.com>
2023-03-15 19:54:30 +0900

    add: swagger 설정

--- src/main/kotlin/com/novel/cloud/web/domain/auth/controller/AuthController.kt
+++ src/main/kotlin/com/novel/cloud/web/domain/auth/controller/AuthController.kt
@@ -4,15 +4,20 @@ import com.novel.cloud.web.domain.auth.controller.rq.OAuthRq
 import com.novel.cloud.web.domain.auth.service.OAuth2Service
 import com.novel.cloud.web.domain.dto.JwtTokenDto
 import com.novel.cloud.web.path.ApiPath
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "인증")
 @RestController
 class AuthController(
     private val oAuth2Service: OAuth2Service
 ) {
+
+    @Operation(summary = "OAuth 로그인")
     @PostMapping(ApiPath.LOGIN_OAUTH2)
     fun loginOAuth2(@Validated @RequestBody rq: OAuthRq): JwtTokenDto {
         return oAuth2Service.loginOAuth2(rq)
--- src/main/kotlin/com/novel/cloud/web/domain/member/controller/MemberController.kt
+++ src/main/kotlin/com/novel/cloud/web/domain/member/controller/MemberController.kt
@@ -4,14 +4,17 @@ import com.novel.cloud.web.config.security.context.MemberContext
 import com.novel.cloud.web.domain.member.controller.rs.FindMemberSelfRs
 import com.novel.cloud.web.domain.member.service.FindMemberService
 import com.novel.cloud.web.path.ApiPath
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-
+@Tag(name = "멤버")
 @RestController
 class MemberController(private val findMemberService: FindMemberService) {
 
+    @Operation(summary = "내 정보 불러오기")
     @GetMapping(ApiPath.MEMBER_SELF)
     fun findMemberSelf(@AuthenticationPrincipal memberContext: MemberContext?): FindMemberSelfRs? {
         return findMemberService.findMemberSelf(memberContext)
