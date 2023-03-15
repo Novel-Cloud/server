429e76a98fdfd943248a0a41671a3bbcebfc3d59
youngmin <min050410@gmail.com>
2023-03-15 20:23:48 +0900

    fix: base64 decoding 이슈

--- src/main/kotlin/com/novel/cloud/web/config/security/jwt/JwtTokenFactory.kt
+++ src/main/kotlin/com/novel/cloud/web/config/security/jwt/JwtTokenFactory.kt
@@ -8,8 +8,6 @@ import com.novel.cloud.web.utils.DateUtils
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
-import io.jsonwebtoken.io.Decoders
-import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
@@ -26,13 +24,12 @@ class JwtTokenFactory {
         val expiredDate = DateUtils.addTime(now, TOKEN_TIME_TO_LIVE)
         val expiredLocalDateTime =
             LocalDateTime.ofInstant(expiredDate.toInstant(), ZoneId.systemDefault())
-        val key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(JwtProperty.SIGN_KEY))
         val token: String = Jwts.builder()
             .setClaims(createJwtClaims(member))
             .setIssuedAt(now)
             .setIssuer(JWT_ISSUER)
             .setExpiration(expiredDate)
-            .signWith(key, SignatureAlgorithm.HS256)
+            .signWith(SignatureAlgorithm.HS256, JwtProperty.SIGN_KEY)
             .compact()
         return JwtTokenDto(
                 token = token,
