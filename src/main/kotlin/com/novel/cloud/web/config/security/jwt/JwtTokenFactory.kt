af34a6ede07973b870372386b2d6e3f11a5d1ffb
youngmin <min050410@gmail.com>
2023-03-15 11:47:53 +0900

    refac: CustomOAuth2SuccessHandler 리팩토링

--- src/main/kotlin/com/novel/cloud/web/config/security/jwt/JwtTokenFactory.kt
+++ src/main/kotlin/com/novel/cloud/web/config/security/jwt/JwtTokenFactory.kt
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 import java.time.ZoneId
 
-
 @Component
 class JwtTokenFactory {
 
--- src/main/kotlin/com/novel/cloud/web/security/CustomOauth2UserService.kt
+++ src/main/kotlin/com/novel/cloud/web/security/CustomOauth2UserService.kt
@@ -1,6 +1,5 @@
 package com.novel.cloud.web.security
 
-import lombok.RequiredArgsConstructor
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException
@@ -8,13 +7,12 @@ import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
-
 @Service
-@RequiredArgsConstructor
 @Transactional
 class CustomOauth2UserService : DefaultOAuth2UserService() {
     @Throws(OAuth2AuthenticationException::class)
     override fun loadUser(userRequest: OAuth2UserRequest): OAuth2User {
-        return super.loadUser(userRequest);
+        return super.loadUser(userRequest)
     }
+
 }
