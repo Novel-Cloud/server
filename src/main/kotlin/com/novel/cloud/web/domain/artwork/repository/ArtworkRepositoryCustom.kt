5b280f7cb4e6b14a292cba03b2c189a3e1b44f6e
min050410 <min050410@gmail.com>
2023-03-20 19:54:22 +0900

    feat(Artwork): 모든 Artwork 보기 querydsl

--- src/main/kotlin/com/novel/cloud/web/domain/artwork/repository/ArtworkRepositoryCustom.kt
+++ src/main/kotlin/com/novel/cloud/web/domain/artwork/repository/ArtworkRepositoryCustom.kt
@@ -3,10 +3,9 @@ package com.novel.cloud.web.domain.artwork.repository
 import com.novel.cloud.db.entity.artwork.Artwork
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.data.jpa.repository.JpaRepository
 
-//interface ArtworkRepositoryCustom {
-//
-//    fun findArtworkList(pageable: Pageable): Page<Artwork>
-//
-//}
+interface ArtworkRepositoryCustom {
+
+    fun findArtworkList(pageable: Pageable): Page<Artwork>
+
+}
--- src/main/kotlin/com/novel/cloud/web/domain/artwork/service/FindArtworkService.kt
+++ src/main/kotlin/com/novel/cloud/web/domain/artwork/service/FindArtworkService.kt
@@ -1,16 +1,20 @@
 package com.novel.cloud.web.domain.artwork.service
 
+import com.novel.cloud.db.entity.member.Member
 import com.novel.cloud.web.config.security.context.MemberContext
+import com.novel.cloud.web.domain.member.service.FindMemberService
 import com.novel.cloud.web.endpoint.Pagination
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
 
 @Service
 @Transactional
-class FindArtworkService {
+class FindArtworkService (
+    val findMemberService: FindMemberService
+) {
 
     fun findAllArtwork(memberContext: MemberContext, pagination: Pagination) {
-
+        val member: Member = findMemberService.findLoginMemberOrElseException(memberContext)
     }
 
 }
