8ef0acf8468fa00da227faddbfef13c980d05f2a
min050410 <min050410@gmail.com>
2023-03-21 12:19:01 +0900

    feat(member): 내 정보 불러오기에 내 작품들 추가

--- src/main/kotlin/com/novel/cloud/web/domain/member/controller/rs/FindMemberSelfRs.kt
+++ src/main/kotlin/com/novel/cloud/web/domain/member/controller/rs/FindMemberSelfRs.kt
@@ -1,21 +1,33 @@
 package com.novel.cloud.web.domain.member.controller.rs
 
+import com.novel.cloud.db.entity.artwork.Artwork
 import com.novel.cloud.db.entity.member.Member
+import com.novel.cloud.web.domain.artwork.controller.rs.FindArtworkRs
 
 data class FindMemberSelfRs(
     val memberId: Long? = null,
     val nickname: String? = null,
     val picture: String? = null,
-    val email: String? = null
+    val email: String? = null,
+    val artworks: List<FindArtworkRs>? = null
 ){
     companion object {
+
         fun create(member: Member?): FindMemberSelfRs {
             return FindMemberSelfRs(
                 memberId = member?.id,
                 nickname = member?.nickname,
                 picture = member?.picture,
-                email = member?.email
+                email = member?.email,
+                artworks = getArtworks(member?.artworks)
             )
         }
+
+        private fun getArtworks(artworks: List<Artwork>?): List<FindArtworkRs>? {
+            return artworks?.map { artwork ->
+                FindArtworkRs.create(artwork)
+            }?.toList()
+        }
+
     }
 }
