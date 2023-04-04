9892ecdb3d63aaf366636a7f5c74d6442ae4e977
min050410 <min050410@gmail.com>
2023-04-04 20:43:46 +0900

    feat(shortcut): 내 단축어 불러오기 Response 변경

--- src/main/kotlin/com/novel/cloud/web/domain/abbreviation/controller/rs/FindSequenceAbbreviationRs.kt
+++ src/main/kotlin/com/novel/cloud/web/domain/abbreviation/controller/rs/FindSequenceAbbreviationRs.kt
@@ -3,13 +3,13 @@ package com.novel.cloud.web.domain.abbreviation.controller.rs
 import com.novel.cloud.db.entity.abbreviation.Abbreviation
 
 data class FindSequenceAbbreviationRs (
-    val id: Long? = null,
+    val shortcutId: Long? = null,
     val content: String? = null,
 ){
     companion object {
         fun create(abbreviation: Abbreviation): FindSequenceAbbreviationRs {
             return FindSequenceAbbreviationRs(
-                id = abbreviation.id,
+                shortcutId = abbreviation.id,
                 content = abbreviation.content
             )
         }
---
+++ src/main/kotlin/com/novel/cloud/web/endpoint/ListResponse.kt
@@ -0,0 +1,7 @@
+package com.novel.cloud.web.endpoint
+
+data class ListResponse<T> (
+    val list: List<T>? = null
+)
+
+
