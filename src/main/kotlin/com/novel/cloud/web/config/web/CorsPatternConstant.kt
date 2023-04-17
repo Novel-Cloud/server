7e07af91b83ddc1bc5f4343bdd560c68abff64fb
min050410 <min050410@gmail.com>
2023-04-17 17:00:44 +0900

    feat(config): cors 패턴 추가

--- src/main/kotlin/com/novel/cloud/web/config/web/CorsPatternConstant.kt
+++ src/main/kotlin/com/novel/cloud/web/config/web/CorsPatternConstant.kt
@@ -6,8 +6,8 @@ object CorsPatternConstant {
 
     const val CORS_LOCAL_SWAGGER = "http*://10.150.151.237/:[*]"
 
-//    const val CORS_BSSM_PR = ""
+    const val CORS_PR = "http*://ec2-54-180-163-47.ap-northeast-2.compute.amazonaws.com"
 
-//    const val CORS_BSSM_PR_FE = ""
+    const val CORS_PR_FE = "http*://novel-cloud.kro.kr"
 
 }
