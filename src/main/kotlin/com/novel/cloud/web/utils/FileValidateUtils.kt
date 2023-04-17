529d34b02803bddba56b864c418b10071378e954
min050410 <min050410@gmail.com>
2023-04-17 22:51:55 +0900

    refac: fileValidateUtil 메서드 오버로딩

--- src/main/kotlin/com/novel/cloud/web/utils/FileValidateUtils.kt
+++ src/main/kotlin/com/novel/cloud/web/utils/FileValidateUtils.kt
@@ -6,7 +6,7 @@ import org.springframework.web.multipart.MultipartFile
 object FileValidateUtils {
 
     fun supportedFileValidationCheck(
-        multipartFile: MultipartFile?,
+        multipartFile: MultipartFile?
     ) {
         if (multipartFile == null || multipartFile.isEmpty) {
             throw FileException("파일이 비어있습니다.")
@@ -15,6 +15,21 @@ object FileValidateUtils {
         if (!isSupportedContentType(contentType)) {
             throw FileException("PNG, JPG, GIF만 허용됩니다.")
         }
+    }
+
+    fun supportedFileValidationCheck(
+        multipartFiles: List<MultipartFile>
+    ) {
+        multipartFiles.map { multipartFile ->
+            if (multipartFile.isEmpty) {
+                throw FileException("파일이 비어있습니다.")
+            }
+            val contentType = multipartFile.contentType
+            if (!isSupportedContentType(contentType)) {
+                throw FileException("PNG, JPG, GIF만 허용됩니다.")
+            }
+        }
+
     }
 
     private fun isSupportedContentType(contentType: String?): Boolean {
